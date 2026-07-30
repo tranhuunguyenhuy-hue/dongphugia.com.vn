fc0374612e452771e64454060d301ce26c274cc5
WuyXuy <m-ac@Mac-mini-cua-Mac.local>
2026-07-31 01:19:23 +0700

    ci: use deterministic Lighthouse throttling

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -14,7 +14,7 @@ module.exports = {
                     deviceScaleFactor: 2,
                     disabled: false,
                 },
-                throttlingMethod: 'devtools',
+                throttlingMethod: 'simulate',
                 onlyCategories: [
                     'performance',
                     'accessibility',
