543bd0ddc6f78419d96c1dc730f1d5ddb553a8db
tranhuunguyenhuy-hue <258872309+tranhuunguyenhuy-hue@users.noreply.github.com>
2026-08-10 19:58:39 +0700

    chore: remove legacy code and research assets

--- scripts/crawl-inax/extract_link.mjs
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-import * as cheerio from 'cheerio';
-
-const html = fs.readFileSync('scripts/crawl-inax/listing-sample.html', 'utf8');
-const $ = cheerio.load(html);
-
-const firstProductLink = $('.product-box-item').first().find('a').attr('href');
-console.log(firstProductLink);
--- src/lib/perf-logger.ts
+++
@@ -1,7 +0,0 @@
-export function logPerf(label: string) {
-    if (process.env.NODE_ENV === 'development') {
-        console.time(label)
-        return () => console.timeEnd(label)
-    }
-    return () => {}
-}
